cee38b231b68b6455feb64f43320f9e066b01d54
Elijah Prince <elijahprince73@gmail.com>
2018-04-11 16:45:27 -0700

    Schema for both Artist and Albums and created querie for getting Artist

--- database/queries/FindArtist.js
+++ database/queries/FindArtist.js
@@ -6,4 +6,6 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the Artist that matches the id
  */
 module.exports = (_id) => {
+	// Can also use findOne({_id: _id})
+	return Artist.findById(_id: _id)
 };
