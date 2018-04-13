38fb83c95209fa53b1943f4520de85517fd13bec
Elijah Prince <elijahprince73@gmail.com>
2018-04-12 22:06:25 -0700

    min and max queries

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,4 +6,34 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+	const minQuery = Artist
+		.find({})
+		.sort({
+			// min
+			age: 1
+		})
+		// Dont do this
+		// .then((artist) => artist[0])
+		// By doing so you are looping through possibly alot of artists and its making a huge request on the server
+		// Use limit instead to only return 1 item rather than looping through a list
+		.limit(1)
+		//returns just one artist from the limit
+		.then((artists) => artists[0].age)
+
+	const maxQuery = Artist
+		.find({})
+		.sort({
+			// max
+			age: -1
+		})
+		.limit(1)
+		.then((artists) => artists[0].age)
+
+	return Promise.all([minQuery, maxQuery])
+		.then((result) => {
+			return {
+				min: result[0],
+				max: result[1]
+			}
+		})
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,4 +6,34 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+	const minQuery = Artist
+		.find({})
+		.sort({
+			// min
+			yearsActive: 1
+		})
+		// Dont do this
+		// .then((artist) => artist[0])
+		// By doing so you are looping through possibly alot of artists and its making a huge request on the server
+		// Use limit instead to only return 1 item rather than looping through a list
+		.limit(1)
+		//returns just one artist from the limit
+		.then((artists) => artists[0].yearsActive)
+
+	const maxQuery = Artist
+		.find({})
+		.sort({
+			// max
+			yearsActive: -1
+		})
+		.limit(1)
+		.then((artists) => artists[0].yearsActive)
+
+	return Promise.all([minQuery, maxQuery])
+		.then((result) => {
+			return {
+				min: result[0],
+				max: result[1]
+			}
+		})
 };
